480cf38d1b2217ccf8443c4fde38b9644291f358
YogaPan <godhand1234567@gmail.com>
2016-03-13 13:11:07 +0800

    change name

--- kernel-build/basic-modules/hello.c
+++ kernel-build/basic-modules/hello.c
@@ -5,17 +5,17 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("YogaPan <godhand1234567@gmail.com>");
 MODULE_DESCRIPTION("Hello!");
 
-static int __init hello_module_init(void)
+static int __init hello_init(void)
 {
 	printk(KERN_DEBUG "Hello World!\n");
 	return 0;
 }
 
-static void __exit hello_module_exit(void)
+static void __exit hello_exit(void)
 {
 	printk(KERN_DEBUG "Good Bye.\n");
 }
 
 
-module_init(hello_module_init);
-module_exit(hello_module_exit);
+module_init(hello_init);
+module_exit(hello_exit);
