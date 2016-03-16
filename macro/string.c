4aa00ea5ce9799b01a23f92711b0b85b187f4aa0
YogaPan <godhand1234567@gmail.com>
2016-03-16 19:54:48 +0800

    Add print_red

--- macro/string.c
+++ macro/string.c
@@ -6,12 +6,19 @@
 #define UNDERLINE_VAR(var)             \
 	int _##var
 
+#define PRINT_RED(str, ...) do {        \
+	printf("\x1b[31m");            \
+	printf(str, ##__VA_ARGS__);           \
+	printf("\033[0m");             \
+} while (0)
+
 int main(void)
 {
 	UNDERLINE_VAR(a);
 
 	_a = 3;
 	PRINT_VAR(_a);
+	PRINT_RED("fuck %s\n", "you");
 
 	return 0;
 }
