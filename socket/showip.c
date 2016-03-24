d840e3166ea0a08a63fd651c02c52b63a2c7d49c
YogaPan <godhand1234567@gmail.com>
2016-03-24 22:05:54 +0800

    show ip

--- socket/showip.c
+++ socket/showip.c
@@ -3,50 +3,49 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
+#define for_each_addrinfo(p, res) \
+	for ((p) = (res); (p) != NULL; (p) = (p)->ai_next)
+
+int main(void)
 {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: showip hostname\n");
-		exit(1);
-	}
-
-	int status;
-	struct addrinfo hints, *res, *p;
+	int ret;
+	const void *addr;
+	const char *ipver;
 	char ipstr[INET6_ADDRSTRLEN];
+	struct addrinfo hints;
+	struct addrinfo *res, *p;
 
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family   = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	status = getaddrinfo(argv[1], NULL, &hints, &res);
-	if (status != 0) {
-		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-		exit(1);
+	ret = getaddrinfo("www.google.com", NULL, &hints, &res);
+	if (ret != 0) {
+		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(ret));
+		return 1;
 	}
 
-	printf("IP address for %s:\n\n", argv[1]);
-
-	for (p = res; p != NULL; p = p->ai_next) {
-		void *addr;
-		char *ipver;
-
+	for_each_addrinfo(p, res) {
 		if (p->ai_family == AF_INET) {
 			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+
 			addr = &(ipv4->sin_addr);
 			ipver = "IPv4";
 		} else {
 			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+
 			addr = &(ipv6->sin6_addr);
 			ipver = "IPv6";
 		}
-
 		inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
-		printf(" %s: %s\n", ipver, ipstr);
+		printf("%s: %s\n", ipver, ipstr);
 	}
 
 	freeaddrinfo(res);
+
 	return 0;
 }
