d43407d2f6e19e039c1b09e11290d53699eba4da
YogaPan <godhand1234567@gmail.com>
2016-04-07 11:21:20 +0800

    Add file

--- c/stdlib/file/read.c
+++ c/stdlib/file/read.c
@@ -27,7 +27,16 @@ int main(int argc, char *argv[])
 void read(FILE *fp)
 {
 	char buf[SIZE];
+	char ch;
 
 	while (fgets(buf, SIZE, fp))
 		printf("%s", buf);
+
+	rewind(fp);
+
+	ch = fgetc(fp);
+	while (ch != EOF) {
+		printf("%c", ch);
+		ch = fgetc(fp);
+	}
 }
