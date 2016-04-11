7ce5f46ecec8580be1f91e4fac1c19b64eb68c1b
YogaPan <godhand1234567@gmail.com>
2016-04-11 16:16:08 +0800

    fix time

--- c/syscall/time/timeclock.c
+++ c/syscall/time/timeclock.c
@@ -2,17 +2,25 @@
 #include <time.h>
 #include <unistd.h>
 
+#define BILLION 1E9
+
 int main(void)
 {
 	struct timespec start, end;
-	int sec;
+	float sec;
 
 	clock_gettime(CLOCK_REALTIME, &start);
 	sleep(2);
 	clock_gettime(CLOCK_REALTIME, &end);
 
 	sec = end.tv_sec - start.tv_sec;
-	printf("%d\n", sec);
+	printf("%f\n", sec);
+
+	clock_gettime(CLOCK_REALTIME, &start);
+	clock_gettime(CLOCK_REALTIME, &end);
+
+	sec = (end.tv_nsec - start.tv_nsec)/ BILLION;
+	printf("%f\n", sec);
 
 	return 0;
 }
