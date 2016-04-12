a68854a212f3d24e349f98398c89141357b69936
YogaPan <godhand1234567@gmail.com>
2016-04-12 16:47:16 +0800

    Add selection sort

--- c/DSA/sort/selection_sort.c
+++ c/DSA/sort/selection_sort.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 
+#define ARRAY_SIZE 10
+
 #define swap(x, y) \
 ({ \
-	
+	typeof(x) __tmp = x; \
+	x = y; \
+	y = __tmp; \
 })
+
+void selection_sort(int *array, int size)
+{
+	int i, j;
+	int min;
+
+	for (i = 0; i < size; i++) {
+		min = i;
+
+		for (j = i + 1; j < size; j++) {
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+			swap(array[min], array[i]);
+	}
+}
 
 int main(void)
 {
+	int i;
+	int array[ARRAY_SIZE] = { 32, 11, 99, 51, 32, 0, 54, 9, 3, 11 };
+
+	selection_sort(array, ARRAY_SIZE);
+	for (i = 0; i < ARRAY_SIZE; i++)
+		printf("%d ", array[i]);
+	printf("\n");
+
 	return 0;
 }
