368b80bd86691341257fd7ae75329a98b80ee03a
YogaPan <godhand1234567@gmail.com>
2016-04-13 17:59:34 +0800

    Add bar

--- python/science/hist.py
+++ python/science/hist.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 
 def main():
     t = np.random.randn(10000)
-    plt.hist(t, 40, normed=1, color='r', alpha=0.5)
-    #  plt.hist(t, 40, color='r', alpha=0.5)
+    plt.hist(t, 40, normed=1, color='b', alpha=0.5)
     plt.title('Test')
     plt.xlabel('X')
     plt.ylabel('Y')
--- python/science/line.py
+++ python/science/line.py
@@ -2,16 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    t1 = np.linspace(3, 8, 9)
-    t2 = np.linspace(0, 32, 9)
+    t1 = np.linspace(0, 7, 10000)
+    t2 = t1 * t1
 
     plt.title('test')
     plt.ylabel('Y')
     plt.xlabel('X')
+    plt.axis([0, 10, 0, 40])
+    plt.plot(t1, t2, 'g-', linewidth=2.0,
+            label='n2')
     #  plt.grid(True)
-    plt.axis([0, 40, 0, 40])
-    lines = plt.plot(t1, t2, color='g', linestyle='-', linewidth=2.0)
-    #  plt.setp(lines, color='r', linewidth=1.0, linestyle='-')
+    plt.legend()
 
     plt.savefig('line.png')
 
