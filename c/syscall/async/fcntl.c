9da5adc9a48ac6c11ed962e765bb0ea180cc928b
YogaPan <godhand1234567@gmail.com>
2016-05-13 03:24:46 +0800

    update fcntl

--- c/syscall/async/fcntl.c
+++ c/syscall/async/fcntl.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(void)
+static int show_status(int fd)
 {
-	int read_fd;
 	int retval;
 
-	read_fd = open("fcntl.c", O_RDONLY);
-	retval = fcntl(read_fd, F_GETFL, 0);
+	retval = fcntl(fd, F_GETFL, 0);
 
 	if (retval < 0) {
 		perror("fcntl");
-		return 0;
+		close(fd);
+		exit(EXIT_FAILURE);
 	}
 
 	switch(retval & O_ACCMODE) {
@@ -25,6 +25,30 @@ int main(void)
 	default:
 		break;
 	}
+
+	if (retval & O_APPEND)
+		printf("append\n");
+
+	return retval;
+}
+
+int main(void)
+{
+	int read_fd;
+	int retval;
+
+	read_fd = open("fcntl.c", O_RDONLY);
+
+	retval = show_status(read_fd);
+
+	retval = fcntl(read_fd, F_SETFL, retval | O_APPEND);
+	if (retval < 0) {
+		perror("fcntl");
+		close(read_fd);
+		return 0;
+	}
+
+	show_status(read_fd);
 
 	close(read_fd);
 
