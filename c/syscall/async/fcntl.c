abf151d25fdf48d5fe279cd6b5d1180969c84685
YogaPan <godhand1234567@gmail.com>
2016-05-13 02:45:21 +0800

    Add fcntl

--- c/syscall/async/fcntl.c
+++ c/syscall/async/fcntl.c
@@ -4,5 +4,29 @@
 
 int main(void)
 {
+	int read_fd;
+	int retval;
+
+	read_fd = open("fcntl.c", O_WRONLY);
+	retval = fcntl(read_fd, F_GETFD, 0);
+
+	if (retval < 0) {
+		perror("fcntl");
+		return 0;
+	}
+
+	switch(retval & O_ACCMODE) {
+	case O_RDONLY:
+		printf("Read only\n");
+		break;
+	case O_WRONLY:
+		printf("write only\n");
+		break;
+	default:
+		break;
+	}
+
+	close(read_fd);
+
 	return 0;
 }
